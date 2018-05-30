746769348d02f26add08e20273e4e2edfb2b9bc9
Norman Xu <im@norm.im>
2018-05-30 19:28:48 +0100

    expose more

--- bundle.js
+++ bundle.js
@@ -1,4 +1,8 @@
 window.huntun = {
-    ScrollBar: require('./src/ScrollBar')
+    ScrollBar: require('./src/ScrollBar'),
+    Prompt: require('./src/Prompt'),
+    Button: require('./src/Button'),
+    Close: require('./src/Close'),
+    Label: require('./src/Label')
 };
 module.exports = huntun;
