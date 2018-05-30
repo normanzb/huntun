940c1bff6d0ae742fe531f2457b929f40967b113
Norman Xu <im@norm.im>
2018-05-30 19:37:12 +0100

    expose more comps

--- bundle.js
+++ bundle.js
@@ -1,8 +1,16 @@
-window.huntun = {
+var huntun = {
     ScrollBar: require('./src/ScrollBar'),
     Prompt: require('./src/Prompt'),
     Button: require('./src/Button'),
     Close: require('./src/Close'),
-    Label: require('./src/Label')
+    Label: require('./src/Label'),
+    fields: {
+        CheckBox: require('./src/fields/CheckBox'),
+        Text: require('./src/fields/Text'),
+        TextButton: require('./src/fields/TextButton')
+    }
 };
+if (typeof window) {
+    window.huntun = huntun;
+}
 module.exports = huntun;
